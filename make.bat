0f8ab05040d1981e855864fd9275d6997a61989d
Lonami Exo <totufals@hotmail.com>
2019-01-17 16:37:09 +0100

    Cleanup directory structure

--- make.bat
+++ make.bat
@@ -1,5 +1,7 @@
 @echo off
-del *.exe > NUL 2>&1
+
+mkdir bin > NUL 2>&1
+del bin\*.exe > NUL 2>&1
 
 set WINKIT=C:\Program Files (x86)\Windows Kits\10
 set WINVER=10.0.17134.0
@@ -7,15 +9,16 @@ set WINVER=10.0.17134.0
 set WINBIN=%WINKIT%\bin\%WINVER%\x64
 set WINLIB=%WINKIT%\Lib\%WINVER%\um\x64
 set WININC=%WINKIT%\Include\%WINVER%\um
+set COMMON=cpp\utils.cpp cpp\point.cpp cpp\color.cpp cpp\input.cpp
 
 clang -Xclang -flto-visibility-public-std ^
   -I"%WININC%" -l"%WINLIB%\User32.lib" -l"%WINLIB%\WS2_32.lib" -l"%WINLIB%\Gdi32.lib" ^
-  utils.cpp point.cpp color.cpp input.cpp poe.cpp -opoe.exe ^
+  -Iinclude %COMMON% cpp\poe.cpp -obin\poe.exe ^
   > build.log 2>&1
 
 clang -Xclang -flto-visibility-public-std ^
   -I"%WININC%" -l"%WINLIB%\User32.lib" -l"%WINLIB%\WS2_32.lib" -l"%WINLIB%\Gdi32.lib" ^
-  utils.cpp point.cpp color.cpp input.cpp memu-helper.cpp -omemu-helper.exe ^
+  -Iinclude %COMMON% cpp\memu-helper.cpp -obin\memu-helper.exe ^
   >> build.log 2>&1
 
-"%WINBIN%\mt.exe" -manifest poe.exe.manifest -outputresource:poe.exe;#1 >> build.log 2>&1
+"%WINBIN%\mt.exe" -manifest poe.exe.manifest -outputresource:bin\poe.exe;#1 >> build.log 2>&1
