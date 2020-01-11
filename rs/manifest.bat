295d6635e625802e7833776f009b01c78e21a115
Lonami Exo <totufals@hotmail.com>
2020-01-11 15:46:11 +0100

    Copy result to root on running manifest

--- rs/manifest.bat
+++ rs/manifest.bat
@@ -4,4 +4,5 @@ set WINKIT=C:\Program Files (x86)\Windows Kits\10
 set WINVER=10.0.18362.0
 set WINBIN=%WINKIT%\bin\%WINVER%\x64
 
-"%WINBIN%\mt.exe" -manifest ..\poe.exe.manifest -outputresource:target\release\poe.exe;#1
+copy target\release\poe.exe poe.exe
+"%WINBIN%\mt.exe" -manifest ..\poe.exe.manifest -outputresource:poe.exe;#1
