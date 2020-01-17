03b99c4fb494cb53133e2c511c1f8de7ed8b42bd
Lonami Exo <totufals@hotmail.com>
2020-01-17 20:30:20 +0100

    Add more non-working code for tooltips

--- rs/src/input/screen.rs
+++ rs/src/input/screen.rs
@@ -6,22 +6,24 @@ use std::io::Error;
 use std::mem::MaybeUninit;
 
 use winapi::um::winuser::{
-    CreateWindowExA, DefWindowProcA, DispatchMessageA, GetClientRect, GetDesktopWindow,
-    GetMessageA, GetWindowRect, RegisterClassExA, SendMessageA, SetCursorPos, SetWindowPos,
-    TranslateMessage, COLOR_WINDOW, CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, HWND_TOPMOST,
-    SWP_NOACTIVATE, SWP_NOMOVE, SWP_NOSIZE, WNDCLASSEXA, WS_EX_TOPMOST, WS_OVERLAPPEDWINDOW,
-    WS_POPUP, WS_VISIBLE,
+    CreateWindowExA, CreateWindowExW, DefWindowProcA, DefWindowProcW, DispatchMessageA,
+    GetClientRect, GetDesktopWindow, GetMessageA, GetWindowRect, RegisterClassExA,
+    RegisterClassExW, SendMessageA, SendMessageW, SetCursorPos, SetWindowPos, TranslateMessage,
+    COLOR_WINDOW, CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, HWND_TOPMOST, SWP_NOACTIVATE, SWP_NOMOVE,
+    SWP_NOSIZE, WNDCLASSEXA, WNDCLASSEXW, WS_EX_TOPMOST, WS_OVERLAPPEDWINDOW, WS_POPUP, WS_VISIBLE,
 };
 
-use winapi::shared::minwindef::{ATOM, DWORD};
+use winapi::shared::minwindef::{ATOM, DWORD, MAKELONG};
 use winapi::shared::windef::{HBRUSH, HWND, RECT};
 use winapi::um::commctrl::{
-    TOOLINFOA, TOOLTIPS_CLASS, TTF_SUBCLASS, TTM_ADDTOOLA, TTM_POPUP, TTS_ALWAYSTIP, TTS_NOPREFIX,
+    TOOLINFOA, TOOLINFOW, TOOLTIPS_CLASS, TTF_ABSOLUTE, TTF_IDISHWND, TTF_SUBCLASS, TTM_ADDTOOLA,
+    TTM_ADDTOOLW, TTM_POPUP, TTM_SETMAXTIPWIDTH, TTM_TRACKACTIVATE, TTM_TRACKPOSITION,
+    TTM_UPDATETIPTEXTW, TTS_ALWAYSTIP, TTS_NOPREFIX,
 };
 use winapi::um::errhandlingapi::GetLastError;
-use winapi::um::libloaderapi::GetModuleHandleA;
+use winapi::um::libloaderapi::{GetModuleHandleA, GetModuleHandleW};
 use winapi::um::wingdi::{GetPixel, CLR_INVALID};
-use winapi::um::winnt::{LPCSTR, LPSTR};
+use winapi::um::winnt::{LPCSTR, LPCWSTR, LPSTR};
 
 /// Gets the primary screen's size as `(width, height)`.
 ///
@@ -143,39 +145,106 @@ pub fn create_window() -> Result<HWND, DWORD> {
 
 pub fn show_tooltip() {
     unsafe {
-        let tooltip = create_window().unwrap();
-
+        // https://stackoverflow.com/q/5896030
         let parent = GetDesktopWindow();
-        let instance = std::ptr::null_mut();
-        let mut ti = TOOLINFOA {
-            cbSize: std::mem::size_of::<TOOLINFOA>() as u32,
-            uFlags: TTF_SUBCLASS,
-            hwnd: parent,
-            hinst: instance,
-            uId: tooltip as usize,
-            lpszText: "tool-tip".as_ptr() as LPSTR,
+        let appHandle = GetModuleHandleW(std::ptr::null());
+
+        let atom = RegisterClassExW(&WNDCLASSEXW {
+            cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(DefWindowProcW),
+            cbClsExtra: 0,
+            cbWndExtra: 0,
+            hInstance: GetModuleHandleW(std::ptr::null_mut()),
+            hIcon: std::ptr::null_mut(),
+            hCursor: std::ptr::null_mut(),
+            hbrBackground: COLOR_WINDOW as HBRUSH,
+            lpszMenuName: std::ptr::null_mut(),
+            lpszClassName: TOOLTIPS_CLASS.as_ptr() as LPCWSTR,
+            hIconSm: std::ptr::null_mut(),
+        });
+
+        let toolTipWnd = CreateWindowExW(
+            WS_EX_TOPMOST,
+            TOOLTIPS_CLASS.as_ptr() as LPCWSTR,
+            std::ptr::null_mut(),
+            WS_POPUP | TTS_NOPREFIX | TTS_ALWAYSTIP,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            parent,
+            std::ptr::null_mut(),
+            appHandle,
+            std::ptr::null_mut(),
+        );
+        dbg!(toolTipWnd);
+
+        let ti = TOOLINFOW {
+            cbSize: std::mem::size_of::<TOOLINFOW>() as u32,
+            uFlags: TTF_ABSOLUTE | TTF_IDISHWND, /* | TTF_TRACK */
+            // Don't specify TTF_TRACK here. Otherwise the tooltip won't show up.
+            hwnd: toolTipWnd,
+            hinst: std::ptr::null_mut(),
+            uId: toolTipWnd as usize,
+            lpszText: "\0\0".as_ptr() as LPSTR,
             lParam: 0,
             lpReserved: std::ptr::null_mut(),
             rect: RECT {
-                left: 200,
-                top: 200,
-                right: 400,
-                bottom: 400,
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
             },
         };
-        //dbg!(GetClientRect(parent, &mut ti.rect));
-        dbg!(SendMessageA(
-            tooltip,
-            TTM_ADDTOOLA,
-            0,
-            &ti as *const TOOLINFOA as isize
-        ));
 
-        dbg!(SetCursorPos(300, 300));
-        dbg!(SendMessageA(tooltip, TTM_POPUP, 0, 0));
+        SendMessageW(
+            toolTipWnd,
+            TTM_ADDTOOLW,
+            0,
+            &ti as *const TOOLINFOW as isize,
+        );
+        SendMessageW(toolTipWnd, TTM_SETMAXTIPWIDTH, 0, 350);
+
+        let ti = TOOLINFOW {
+            cbSize: std::mem::size_of::<TOOLINFOW>() as u32,
+            hwnd: toolTipWnd,
+            uId: toolTipWnd as usize,
+            lpszText: "Sample Tip Text\0\0".as_ptr() as LPSTR,
+            hinst: std::ptr::null_mut(),
+            lParam: 0,
+            lpReserved: std::ptr::null_mut(),
+            rect: RECT {
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+            },
+            uFlags: 0,
+        };
+
+        SendMessageW(
+            toolTipWnd,
+            TTM_UPDATETIPTEXTW,
+            0,
+            &ti as *const TOOLINFOW as isize,
+        ); // This will update the tooltip content.
+        SendMessageW(
+            toolTipWnd,
+            TTM_TRACKACTIVATE,
+            1,
+            &ti as *const TOOLINFOW as isize,
+        );
+        SendMessageW(
+            toolTipWnd,
+            TTM_TRACKPOSITION,
+            0,
+            MAKELONG(300, 300) as isize,
+        ); // Update the position of your tooltip. Screen coordinate.
+        SendMessageW(toolTipWnd, TTM_POPUP, 0, 0); // TTM_POPUP not working.. Don't know why.
 
         let mut msg = std::mem::zeroed();
-        while GetMessageA(&mut msg, tooltip, 0, 0) != -1 {
+        while GetMessageA(&mut msg, toolTipWnd, 0, 0) != -1 {
             dbg!(msg.message);
             TranslateMessage(&msg);
             DispatchMessageA(&msg);
