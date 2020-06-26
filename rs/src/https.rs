1d7c41bb137fed28ecf4697619ad7dd0fe0572ce
Lonami Exo <totufals@hotmail.com>
2020-06-26 17:31:57 +0200

    Use attohttpc instead of hand-rolled HTTP queries

--- rs/src/https.rs
+++ rs/src/https.rs
@@ -1,9 +1,3 @@
-use libflate::gzip;
-
-use native_tls::{TlsConnector, TlsStream};
-use std::io::{Read, Write};
-use std::net::TcpStream;
-
 // Possible optimizations:
 // * pre-calculate serialized data length and use with capacity
 // * avoid strings and just use a byte buffer
@@ -11,172 +5,17 @@ use std::net::TcpStream;
 
 // TODO Don't hardcode league, fetch from http://api.pathofexile.com/leagues
 
-fn quote_plus(string: &str) -> String {
-    let mut buffer = [0; 4];
-    let mut result = String::with_capacity(string.len());
-    string.chars().for_each(|c| match c {
-        ' ' => result.push('+'),
-        '-' | '.' | '_' | '~' => result.push(c),
-        '0'..='9' => result.push(c),
-        'A'..='Z' => result.push(c),
-        'a'..='z' => result.push(c),
-        _ => {
-            c.encode_utf8(&mut buffer).as_bytes().iter().for_each(|b| {
-                result.push_str(&format!("%{:x}", b));
-            });
-        }
-    });
-    result
-}
-
-fn url_encode(data: &[(&str, &str)]) -> String {
-    let mut result = String::new();
-    if data.len() > 0 {
-        let (key, value) = data[0];
-        result.push_str(&format!("{}={}", key, quote_plus(value)));
-
-        for (key, value) in &data[1..] {
-            result.push_str(&format!("&{}={}", key, quote_plus(value)));
-        }
-    }
-    result
-}
-
-fn connect_to_poe_trade() -> Result<TlsStream<TcpStream>, String> {
-    let connector = TlsConnector::new().map_err(|e| format!("{:?}", e))?;
-    let stream = TcpStream::connect("poe.trade:443").map_err(|e| format!("{:?}", e))?;
-    let stream = connector
-        .connect("poe.trade", stream)
-        .map_err(|e| format!("{:?}", e))?;
-
-    Ok(stream)
-}
-
-fn post<S: Read + Write>(stream: &mut S, data: &[(&str, &str)]) -> Result<String, String> {
-    let payload = url_encode(data);
-
-    let request = format!("POST /search HTTP/1.1\r
-Host: poe.trade\r
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0\r
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r
-Accept-Language: en\r
-Accept-Encoding: gzip, deflate, br\r
-Content-Type: application/x-www-form-urlencoded\r
-Content-Length: {len}\r
-Origin: https://poe.trade\r
-Connection: keep-alive\r
-Referer: https://poe.trade/\r
-Cookie: __vrz=1.16.10; league=Hardcore%20Metamorph; live_notify_sound=0; live_notify_browser=0; live_frequency=0\r
-Upgrade-Insecure-Requests: 1\r
-\r
-{payload}", len = payload.len(), payload = payload);
-
-    stream
-        .write_all(request.as_bytes())
-        .map_err(|e| format!("{:?}", e))?;
-
-    let mut response = vec![0u8; 4096];
-    let read = stream.read(&mut response).map_err(|e| format!("{:?}", e))?;
-    Ok(String::from_utf8_lossy(&response[..read]).to_string())
-}
-
-fn find_redirect(string: &str) -> Option<String> {
-    if let Some(start) = string.find("Location:") {
-        let start = start + 10;
-        if let Some(end) = string[start..].find("\r\n") {
-            let end = start + end;
-            return Some(string[start..end].into());
-        }
-    }
-    None
-}
-
-fn get<S: Read + Write>(stream: &mut S, path: &str) -> Result<String, String> {
-    let request = format!("GET {path} HTTP/1.1\r
-Host: poe.trade\r
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0\r
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r
-Accept-Language: en\r
-Accept-Encoding: gzip, deflate, br\r
-Referer: https://poe.trade/\r
-Connection: close\r
-Cookie: __vrz=1.16.10; league=Hardcore%20Metamorph; live_notify_sound=0; live_notify_browser=0; live_frequency=0\r
-Upgrade-Insecure-Requests: 1\r
-\r
-", path = path);
-
-    stream
-        .write_all(request.as_bytes())
-        .map_err(|e| format!("{:?}", e))?;
-
-    let mut response = vec![];
-    stream
-        .read_to_end(&mut response)
-        .map_err(|e| format!("{:?}", e))?;
-
-    let mut len_start = match (0..response.len() - 4).find(|&i| &response[i..i + 4] == b"\r\n\r\n")
-    {
-        Some(x) => x + 4, // skip b"\r\n\r\n"
-        None => return Err("could not find empty newline in response".into()),
-    };
-
-    let mut gzipped: Vec<u8> = Vec::with_capacity(response.len() - len_start);
-    loop {
-        // Find the hexadecimal length in the current line
-        let len_end =
-            match (len_start..response.len() - 2).find(|&i| &response[i..i + 2] == b"\r\n") {
-                Some(x) => x,
-                None => return Err("could not find end of chunked length in response".into()),
-            };
-
-        // Parse the length and break if it's 0
-        let len_str = String::from_utf8_lossy(&response[len_start..len_end]);
-        let chunk_len = usize::from_str_radix(&len_str, 16)
-            .map_err(|e| format!("{:?} is not a valid hex number: {:?}", len_str, e))?;
-
-        if chunk_len == 0 {
-            break;
-        }
-
-        // Save the data in its own buffer
-        let data_start = len_end + 2; // skip b"\r\n"
-        let data_end = data_start + chunk_len;
-        gzipped.extend(&response[data_start..data_end]);
-
-        // Prepare the start offset for the next iteration
-        len_start = data_end + 2; // skip b"\r\n"
-        if len_start >= response.len() {
-            return Err("reached eof without finishing the data".into());
-        }
-    }
-
-    let mut decoder = gzip::Decoder::new(&gzipped[..]).map_err(|e| format!("{:?}", e))?;
-    let mut result = Vec::new();
-    decoder
-        .read_to_end(&mut result)
-        .map_err(|e| format!("{:?}", e))?;
-
-    Ok(String::from_utf8_lossy(&result).to_string())
-}
-
 fn query_poe_trade(data: &[(&str, &str)]) -> Result<String, String> {
-    let mut stream = connect_to_poe_trade()?;
-    let response = post(&mut stream, data)?;
-    let redirect = match find_redirect(&response) {
-        Some(x) => x,
-        None => {
-            return Err(format!(
-                "no redirect location found in response:\n{}",
-                response
-            ))
-        }
-    };
-    let path = match redirect.find("poe.trade") {
-        Some(pos) => &redirect[pos + 9..],
-        None => return Err(format!("bad redirect lacks hostname: {}", redirect)),
-    };
+    let response = attohttpc::post("https://poe.trade/search")
+        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0")
+        .header("Cookie", " __vrz=1.16.10; league=Hardcore%20Metamorph; live_notify_sound=0; live_notify_browser=0; live_frequency=0")
+        .params(data)
+        .send()
+        .map_err(|e| format!("{:?}", e))?
+        .bytes()
+        .map_err(|e| format!("{:?}", e))?;
 
-    get(&mut stream, path)
+    Ok(String::from_utf8_lossy(&response).to_string())
 }
 
 fn parse_currency(string: &str) -> Option<f64> {
