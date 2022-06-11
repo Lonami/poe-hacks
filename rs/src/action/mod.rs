632520e3761ca5fbca113108e5314e6df506a4d3
Lonami Exo <totufals@hotmail.com>
2022-06-11 11:05:31 +0200

    Move Checker code into its own file

--- rs/src/action/mod.rs
+++ rs/src/action/mod.rs
@@ -1,7 +1,9 @@
 mod action;
+mod checker;
 mod post;
 mod pre;
 
 pub use action::ActionSet;
+pub use checker::{Checker, Health, Mana, MemoryChecker};
 pub use post::PostCondition;
-pub use pre::{Checker, Health, Mana, MemoryChecker, PreCondition};
+pub use pre::PreCondition;
