3dce1df98c804584788bf12bc3fe305e23046641
Lonami Exo <totufals@hotmail.com>
2022-06-11 10:44:31 +0200

    Update Windows Kit version

--- rs/manifest.bat
+++ rs/manifest.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set WINKIT=C:\Program Files (x86)\Windows Kits\10
-set WINVER=10.0.18362.0
+set WINVER=10.0.22000.0
 set WINBIN=%WINKIT%\bin\%WINVER%\x64
 
 copy target\release\poe.exe poe.exe
